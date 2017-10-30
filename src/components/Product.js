554ca7e4f1183a757e1125a76a7f67140671ce18
stopvoldemort <david.squires1@gmail.com>
2017-10-30 18:03:43 -0400

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,34 @@
 // Code Product Component Here
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class Product extends React.Component {
+  render() {
+    return (
+      <h1>Hi</h1>
+    )
+  }
+}
+
+Product.defaultProps = {
+  hasWatermark: false,
+  size: 'regular'
+}
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+  weight: PropTypes.number.isRequired,
+  customProp: function(props) {
+    if (props.weight < 80 || props.weight > 300 ) {
+      return new Error(
+        'That weight is not valid.'
+      );
+    }
+  }
+};
+
+
+export default Product
